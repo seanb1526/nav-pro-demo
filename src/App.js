5b62c34e1b449485268420704b0ee7c03847111d
Sean Berndlmaier <138079299+seanb1526@users.noreply.github.com>
2024-06-18 11:01:45 -0400

    message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import './Navbar.js';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>IndoorNav Pro Prototype</h1>
+        <p>This is an example of a simple map app we can build for your event</p>
       </header>
+      <body className="App-body">
+        <p>Below is an example of one of our Pro maps, the tier above our basic map.</p>
+        <p>It features locations, points of interest, wayfinding/directions, colors, images, and descriptions. </p>
+        <p>Our Basic tier has locations and wayfinding, but does not include the customization features of images, colors, and points of interest.</p>
+        <iframe
+          title="map"
+          id="myFrame"
+          href="https://www.mappedin.com/"
+          width="80%"
+          height="600"
+          src="https://app.mappedin.com/map/663e34658004177ee42bce3b?floor=m_f61d50016e33e723&utm_medium=email&_hsenc=p2ANqtz-_Jk3Dwc0xMPRhPeOP2X_n9RTqQl771BP6cPGqE-fkixvzKYh4XpASZi5FEiXYCdg-THdtogx3OHYyqkUKFonMPcKtBvFAejY53VIfIUMFqHIaQUl0&_hsmi=310348920&utm_content=310348920&utm_source=hs_email"
+          
+        ></iframe>
+      </body>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,19 @@
+// src/Navbar.js
+import React from 'react';
+import './Navbar.css'; // Import the CSS file for styling
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">MyApp</div>
+      <ul className="navbar-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
