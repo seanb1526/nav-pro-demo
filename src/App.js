e3eaf6a0dfe00e057536ab9f465ec25afa77af3c
Sean Berndlmaier <138079299+seanb1526@users.noreply.github.com>
2024-06-18 11:31:54 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
-import './Navbar.js';
+import Navbar from './navbar.js';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <header className="App-header">
+      <header className="App-header" id="home">
         <h1>IndoorNav Pro Prototype</h1>
         <p>This is an example of a simple map app we can build for your event</p>
       </header>
+
       <body className="App-body">
-        <p>Below is an example of one of our Pro maps, the tier above our basic map.</p>
-        <p>It features locations, points of interest, wayfinding/directions, colors, images, and descriptions. </p>
-        <p>Our Basic tier has locations and wayfinding, but does not include the customization features of images, colors, and points of interest.</p>
+        <p style={{ fontSize: '18px' }} >Below is an example of one of our Pro maps, the tier above our basic map.</p>
+        <p style={{ fontSize: '18px' }} >It features locations, points of interest, wayfinding/directions, colors, images, and descriptions. </p>
+        <p style={{ fontSize: '18px' }} >Our Basic tier has locations and wayfinding, but does not include the customization features of images, colors, and points of interest.</p>
         <iframe
           title="map"
           id="myFrame"
@@ -22,7 +23,14 @@ function App() {
           src="https://app.mappedin.com/map/663e34658004177ee42bce3b?floor=m_f61d50016e33e723&utm_medium=email&_hsenc=p2ANqtz-_Jk3Dwc0xMPRhPeOP2X_n9RTqQl771BP6cPGqE-fkixvzKYh4XpASZi5FEiXYCdg-THdtogx3OHYyqkUKFonMPcKtBvFAejY53VIfIUMFqHIaQUl0&_hsmi=310348920&utm_content=310348920&utm_source=hs_email"
           
         ></iframe>
+        <iframe class="w-100 tour-iframe d-block m-0"
+        src="https://app.mappedin.com/map/660311437c0c4fe5b4cc4758?"
+        height="600"
+        width="80%"
+        title="map2"
+        id="myFrame"></iframe>
       </body>
+
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
       <div className="navbar-logo">MyApp</div>
       <ul className="navbar-links">
         <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
+        <li><a href="#myFrame">Map</a></li>
         <li><a href="#services">Services</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
